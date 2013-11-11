9cd1198f3ccc47baea1b8444aa59038d748b26ad
James Lal <james@lightsofapollo.com>
2013-11-11 03:17:18 -0800

    add utilities for getting the sha1 of a file

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,11 +1,43 @@
+/**
+Top level test fixtures used by all other tests. Handles details like what
+server we are hitting and some grunt work like getting the sha1 to verify
+that we download sane things.
+*/
+
 var fsPath = require('path'),
-    url = require('url');
+    url = require('url'),
+    http = require('http');
 
 var DEFAULTS = Object.freeze({
   url: 'http://localhost:60107'
 });
 
 var options = module.exports.options = DEFAULTS;
+
+
+/**
+Get the sha1 of a given file by downloading it from the expected path.
+This will break in bad ways unless the vagrant server is used.
+
+@param {String} path for binary (not sha1)
+@param {Function} callback for sha1 [Error, String]
+*/
+module.exports.sha1 = function(path, callback) {
+  http.get(module.exports.url(path) + '.sha1', function(res) {
+    var buffer;
+    res.on('data', function(content) {
+      buffer = buffer || content;
+      buffer += content;
+    });
+
+    res.once('error', callback);
+
+    res.once('end', function() {
+      var sha1 = buffer.split(' ').shift().trim();
+      callback(null, sha1);
+    });
+  });
+};
 
 /**
 Returns a url for a given path according to the current test fixture
@@ -19,4 +51,4 @@ module.exports.url = function() {
   parsed.pathname = fsPath.join.apply(fsPath, arguments);
 
   return url.format(parsed);
-}
+};
--- test/fixtures_test.js
+++ test/fixtures_test.js
@@ -1,7 +1,8 @@
 suite('fixtures', function() {
   var http = require('superagent'),
       fixtures = require('./fixtures'),
-      sha1sumPath = 'firefox.tar.bz2.sha1';
+      binary = 'firefox.tar.bz2',
+      sha1sumPath = binary + '.sha1';
 
   test('.url', function() {
     var output = fixtures.url(sha1sumPath);
@@ -12,13 +13,34 @@ suite('fixtures', function() {
   });
 
   suite('sha1', function() {
-    var expected,
-        binary = 'firefox.tar.bz2';
+    var expected;
 
-    //setup(function(done) {
-    //});
+    setup(function(done) {
+      http.get(fixtures.url(sha1sumPath), function(res) {
+        var buffer;
+        res.on('data', function(content) {
+          buffer = buffer || content;
+          buffer += content;
+        });
 
-    test('', function() {
+        res.once('error', done);
+
+        res.on('end', function() {
+          expected = buffer.split(' ').shift().trim();
+          assert(expected, 'fixture sha1 is available');
+          assert.equal(typeof expected, 'string', 'sha1 is a string');
+          assert(expected.length > 0, 'has a length');
+          done();
+        });
+      });
+    });
+
+    test('sha1', function(done) {
+      fixtures.sha1(binary, function(err, sha1) {
+        if (err) return done(err);
+        assert.equal(expected, sha1);
+        done();
+      });
     });
   });
 });
